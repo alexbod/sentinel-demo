f64381b76102596020eb54f180ec5618030eb1ad
Alex Bod <alex@alexbod.com>
2020-09-11 17:57:28 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,66 @@
+terraform {
+  required_version = ">= 0.11.7"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+   description = "Name of the bucket to create"
+}
+
+variable "bucket_acl" {
+   description = "ACL for S3 bucket: private, public-read, public-read-write, etc"
+   default = "private"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.bucket_name}"
+  acl    = "${var.bucket_acl}" 
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "arn:aws:kms:us-east-1:753646501470:key/00c892e8-40c4-4048-a650-0f755876503d"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+ 
+  versioning {
+    enabled = true
+    mfa_delete = true
+  }
+  
+  logging {
+    target_bucket = "roger-bucket"
+  }
+  
+  tags {
+    name        = "Test"
+    Owner = "roger@test.com"
+    website = "true"
+    application_id = "456"
+    stack_name = "test"
+    description = "test"
+    termination_date = "06/29/2020"
+    created_by = "test"
+    data_class = "protected"
+    environment = "dev" 
+  }
+}
+
+output "sse" {
+  value = "${aws_s3_bucket.bucket.server_side_encryption_configuration.0.rule.0.apply_server_side_encryption_by_default.0.sse_algorithm}"
+}
+
+/*
 provider "aws" {
   region = "us-east-1"
 }
@@ -16,4 +79,4 @@ resource "aws_s3_bucket" "b" {
     Name        = "My bucket"
     Environment = "Dev"
   }
-}
+}*/
