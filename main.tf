7b1f05c32462a2eac309ee43ffa4ed86bf12e6e7
Alex Bod <alex@alexbod.com>
2020-10-06 17:14:05 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,15 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
   
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+  
   /*
   server_side_encryption_configuration {
     rule {
