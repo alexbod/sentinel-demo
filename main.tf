91d767faabe53337e5929b8c1efd34ee9e0472ea
Alex Bod <alex@alexbod.com>
2020-10-27 17:54:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_flow_log" "vpc_flow_log" {
   # until we have a CloudWatch Log Group Resource
   log_group_name = "${aws_cloudwatch_log_group.vpc_flow_log_group.name}"
   iam_role_arn = "${aws_iam_role.vpc_flow_logs_role.arn}"
-  vpc_id = aws_vpc.main.id
+  vpc_id = "some_vpc_id"
   traffic_type = "ALL"
 }
 
