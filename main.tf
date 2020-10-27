aa6cf5c375086fba9bac68bc292283de31a5ceab
Alex Bod <alex@alexbod.com>
2020-10-27 16:38:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+
+
+
+
+
 /*
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
