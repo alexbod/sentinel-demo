0ca9f250a2711343d4e623bfd2dbddc063ea1bb9
Alex Bod <alex@alexbod.com>
2020-10-28 16:15:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-3123kjasd098213kjasd98213"
+  acl    = "public-read"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
