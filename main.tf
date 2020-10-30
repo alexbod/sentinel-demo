630629d5161d75d74ca80fdc19784b2b1a30f6c4
Alex Bod <alex@alexbod.com>
2020-10-30 17:25:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,18 @@ provider "aws" {
 }
 
 
+resource "aws_iam_access_key" "lb" {
+  user    = aws_iam_user.lb.name
+  pgp_key = "keybase:some_person_that_exists"
+}
+
+
+/*
 resource "aws_kms_key" "a" {
   description             = "KMS key 1"
   deletion_window_in_days = 10
 }
 
-/*
 
 
 
