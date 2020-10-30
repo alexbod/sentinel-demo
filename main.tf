dda5fd6b1e9a421d71fa8ea0b7eb88e8153be398
Alex Bod <alex@alexbod.com>
2020-10-30 17:30:02 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,41 +2,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_iam_access_key" "lb" {
-  user    = aws_iam_user.lb.name
-  pgp_key = "keybase:some_person_that_exists"
+variable "create_user" { }
+variable "public_key" { }
+
+resource "aws_iam_user" "u" {
+  count = "${var.create_user}"
+  name  = "user_name"
+  path  = "/"
 }
 
-resource "aws_iam_user" "lb" {
-  name = "loadbalancer"
-  path = "/system/"
+resource "aws_iam_access_key" "k" {
+  count   = "${var.create_user}"
+  user    = "${aws_iam_user.u.name}"
+  pgp_key = "${var.public_key}"
 }
 
-resource "aws_iam_user_policy" "lb_ro" {
-  name = "test"
-  user = aws_iam_user.lb.name
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:Describe*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
+output "okta_user_encrypted_secret" {
+  value = "${aws_iam_access_key.k.encrypted_secret}"
 }
-EOF
-}
-
-output "secret" {
-  value = aws_iam_access_key.lb.encrypted_secret
-}
-
-
 
 /*
 resource "aws_kms_key" "a" {
