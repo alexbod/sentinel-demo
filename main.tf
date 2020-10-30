137a0473a8ff540682daff647eeeec271083b0a1
Alex Bod <alex@alexbod.com>
2020-10-30 15:30:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-/*
+
 resource "aws_kms_key" "a" {
   description             = "KMS key 1"
   deletion_window_in_days = 10
   enable_key_rotation = true
 }
 
-
+/*
 
 
 
