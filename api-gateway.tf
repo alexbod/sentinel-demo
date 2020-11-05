a2b865c3e0295eab979222b4be7c06a04d645da1
Alex Bod <alex@alexbod.com>
2020-11-05 16:23:37 +0200

    Update api-gateway.tf

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-
 resource "aws_api_gateway_rest_api" "test" {
   name        = "MyDemoAPI"
   description = "This is my API for demonstration purposes"
