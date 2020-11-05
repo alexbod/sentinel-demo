85bd107718fc55eff641ad16603453c52538ae24
Alex Bod <alex@alexbod.com>
2020-11-05 15:58:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,61 @@ resource "aws_api_gateway_rest_api" "test" {
   description = "This is my API for demonstration purposes"
 }
 
+resource "aws_api_gateway_rest_api" "rest_api" {
+  name        = "ProviderBugGateway"
+  description = "This is my API for demonstration purposes"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/xml" = <<EOF
+{
+   "body" : $input.json('$')
+}
+EOF
+  }
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_integration.integration]
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  stage_name  = "dev"
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_settings" "method_settings" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  stage_name  = aws_api_gateway_stage.stage.stage_name
+  method_path = "${aws_api_gateway_resource.resource.path_part}/${aws_api_gateway_method.method.http_method}"
+
+  settings {
+    caching_enabled = false
+  }
+}
+
 
 /*
 
