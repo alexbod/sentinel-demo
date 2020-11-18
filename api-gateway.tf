17cf6b0ec2945b766fd149258d36b9bdaeb5410a
Alex Bod <alex@alexbod.com>
2020-11-18 16:16:33 +0200

    Update api-gateway.tf

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,9 +1,9 @@
-/*
+
 resource "aws_api_gateway_rest_api" "test" {
   name        = "MyDemoAPI"
   description = "This is my API for demonstration purposes"
 }
-
+/*
 resource "aws_api_gateway_rest_api" "testtwo" {
   name        = "MyDemoAPI"
   description = "This is my API for demonstration purposes"
