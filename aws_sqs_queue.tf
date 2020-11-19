e0419b45501a7d2316da79e8c77f15b57331cb3a
Alex Bod <alex@alexbod.com>
2020-11-19 17:29:30 +0200

    Update aws_sqs_queue.tf

--- aws_sqs_queue.tf
+++ aws_sqs_queue.tf
@@ -12,6 +12,6 @@ resource "aws_sqs_queue" "terraform_queue_no_enc" {
 
 resource "aws_sqs_queue" "terraform_queue_no_enc_one" {
   name                              = "terraform-example-queue-no-enc-one"
-  #kms_master_key_id                 = "alias/aws/sqs"
-  #kms_data_key_reuse_period_seconds = 300
+  kms_master_key_id                 = "alias/aws/sqs"
+  kms_data_key_reuse_period_seconds = 300
 }
