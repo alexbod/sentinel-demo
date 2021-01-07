78ed9b6d665d7ff9c5bc08f4d0b5835161c7761d
Alex Bod <alex@alexbod.com>
2021-01-07 18:15:55 +0200

    Update aws_kinesis_fds.tf

--- aws_kinesis_fds.tf
+++ aws_kinesis_fds.tf
@@ -34,6 +34,27 @@ EOF
 
 resource "aws_kinesis_firehose_delivery_stream" "test_stream" {
   name        = "terraform-kinesis-firehose-test-stream"
+  destination = "splunk"
+
+  s3_configuration {
+    role_arn           = "${aws_iam_role.firehose.arn}"
+    bucket_arn         = "${aws_s3_bucket.bucket.arn}"
+    buffer_size        = 10
+    buffer_interval    = 400
+    compression_format = "GZIP"
+  }
+
+  splunk_configuration {
+    hec_endpoint               = "https://http-inputs-mydomain.splunkcloud.com:443"
+    hec_token                  = "51D4DA16-C61B-4F5F-8EC7-ED4301342A4A"
+    hec_acknowledgment_timeout = 600
+    hec_endpoint_type          = "Event"
+    s3_backup_mode             = "FailedEventsOnly"
+  }
+}
+
+/*resource "aws_kinesis_firehose_delivery_stream" "test_stream" {
+  name        = "terraform-kinesis-firehose-test-stream"
   destination = "http_endpoint"
 
   s3_configuration {
@@ -67,4 +88,4 @@ resource "aws_kinesis_firehose_delivery_stream" "test_stream" {
       }
     }
   }
-}
+}*/
