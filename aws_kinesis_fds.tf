8c09bf10504244386c42f4e2e61e0b590f0184c7
Alex Bod <alex@alexbod.com>
2021-01-07 18:13:30 +0200

    Update aws_kinesis_fds.tf

--- aws_kinesis_fds.tf
+++ aws_kinesis_fds.tf
@@ -1,3 +1,13 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
 resource "aws_iam_role" "firehose" {
   name = "test_role"
 
